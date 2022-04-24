ee514fad833289a49de440b54691423023435dfc
kyu05 <kyu05017@naver.com>
2022-04-24 13:17:22 +0900

    2022-04-24 13:17

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,16 +2,29 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ItemRepository  extends JpaRepository<Item,Long>{
 
     List<Item> findByItemNm(String itemNm);
+    //findBy변수명 - 조회하기
 
     List<Item> findByItemNmOrItemDetail(String itemNm,String itemDetail);
+    //findBy변수명Or변수명 - 두가지 조회하기
 
     List<Item> findByPriceLessThan(Integer price);
+    //findBy변수명LessThen(인수) - 변수명 < 인수
 
     List<Item>  findByPriceLessThanOrderByPriceDesc(Integer price);
+    //findBy변수명LessThenOrderBy변수명DESC(인수) - 인수보다 작은 변수를 내림차순으로 정렬
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+//    @Query("select i from Item i where i.itemDetail like " +
+//            "%:itemDetail% order by i.price desc")
+//    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
