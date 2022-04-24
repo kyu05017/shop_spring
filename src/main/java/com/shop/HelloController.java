55a229cfaaa8a2f2c9b86db23129835480cf2775
kyu05 <kyu05017@naver.com>
2022-04-24 10:05:34 +0900

    스프링부틍

--- src/main/java/com/shop/HelloController.java
+++ src/main/java/com/shop/HelloController.java
@@ -1,4 +1,16 @@
 package com.shop;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("hello")
+    public List<String> hello() {
+        return Arrays.asList("강사님 성공했습니다.", "ㅜㅜㅠㅜㅠㅜㅠㅠ");
+    }
 }
